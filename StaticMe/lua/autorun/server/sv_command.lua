dfeb9ba21b03927c347c707be2056869894899fc
Millzy <millzyyt08@gmail.com>
2022-12-09 21:39:45 +0000

    command handler

---
+++ StaticMe/lua/autorun/server/sv_command.lua
@@ -0,0 +1,34 @@
+local commands = {}
+
+/* ========================== COMMAND HANDLER ========================== */
+
+local function handle_command(sender, text)
+    if (text:find("^/") ~= nil) then 
+        args = {}
+
+        for w in text:gmatch("%w+") do 
+            table.insert(args, w)
+        end
+
+        command = string.sub(args[1], 1, -1)    
+        table.remove(args, 1)
+
+        exec = commands[command]
+
+        if (exec == nil) then
+            return text
+        end
+
+        exec(sender, args)
+
+        return ""
+    end
+
+    return text
+end
+
+local function player_say(sender, text, _) 
+    return handle_command(sender, text)
+end
+
+hook.Add("PlayerSay", "StaticMe:CommandHandler", player_say)
